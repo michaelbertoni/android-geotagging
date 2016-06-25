3e5a789143fabd2af45fa18eb8d002191bd39d99
Michaël BERTONI <michael@thynkio.com>
2016-06-25 19:39:17 +0200

    Essai GridView avec photos depuis carte sd

--- app/src/main/java/baptistin/com/applicationsolo/MainActivity.java
+++ app/src/main/java/baptistin/com/applicationsolo/MainActivity.java
@@ -10,15 +10,27 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import java.io.File;
 import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
     private GridView gridView;
     private GridViewAdapter gridAdapter;
+    ArrayList<String> f = new ArrayList<String>();// list of file paths
+    File[] listFile;
+    ImageLoader imageLoader = ImageLoader.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this).build();
+        ImageLoader.getInstance().init(config);
+        getFromSdcard();
+
+
         setContentView(R.layout.galerie_main);
 
         gridView = (GridView) findViewById(R.id.gridView);
@@ -43,11 +55,33 @@ public class MainActivity extends ActionBarActivity {
     // Prepare some dummy data for gridview
     private ArrayList<ImageItem> getData() {
         final ArrayList<ImageItem> imageItems = new ArrayList<>();
-        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
-        for (int i = 0; i < imgs.length(); i++) {
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
+        // TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
+        for (int i = 0; i < f.size(); i++) {
+            Bitmap bitmap = imageLoader.loadImageSync("file://" + f.get(i));;
             imageItems.add(new ImageItem(bitmap, "Image#" + i));
         }
         return imageItems;
     }
+
+
+    public void getFromSdcard()
+    {
+        File file= new File(android.os.Environment.getExternalStorageDirectory(),"DCIM/100ANDRO");
+
+        if (file.isDirectory())
+        {
+            listFile = file.listFiles();
+
+
+            for (int i = 0; i < listFile.length; i++)
+            {
+
+                f.add(listFile[i].getAbsolutePath());
+
+            }
+        }
+    }
+
+
+
 }
--- app/src/main/java/epsi/com/spacepic/MapsActivity.java
+++ app/src/main/java/epsi/com/spacepic/MapsActivity.java
@@ -14,6 +14,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import baptistin.com.applicationsolo.R;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
